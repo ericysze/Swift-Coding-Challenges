dbc5783e4a98fb745d7fc81b82b42f56acd31fcb
ericysze <ericysze@gmail.com>
2017-01-02 23:03:09 -0500

    Challenge 1 completed, test cases passed

--- Swift Coding Challenges.playground/Contents.swift
+++ Swift Coding Challenges.playground/Contents.swift
@@ -2,4 +2,23 @@
 
 import UIKit
 
-var str = "Hello, playground"
+func challenge1(input: String) -> Bool {
+    
+    var arr = Set<Character>()
+    for i in input.characters {
+        if arr.contains(i) {
+            return false
+        } else {
+            arr.insert(i)
+        }
+    }
+    return true
+}
+
+assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
+assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
+assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
+assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
+
+assert(challenge1(input: "aAa") == false, "Challenge 1 failed")
+
