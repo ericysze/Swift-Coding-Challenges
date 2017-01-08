e3ae3eca7e25cbe293900106383d9f8255d4efd8
ericysze <ericysze@gmail.com>
2017-01-07 23:42:44 -0500

    Challenge 4 complete

--- Swift Coding Challenges.playground/Pages/Challenge 4.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Challenge 4.xcplaygroundpage/Contents.swift
@@ -5,5 +5,13 @@ import Foundation
 var str = "Hello, playground"
 
 extension String {
-    func 
+    func fuzzyContains(string: String) -> Bool {
+        // create an extension of String, and use swift's built in range to check if the input exists within the checked string
+        // both solutions work, however calling '.caseInsensitive' has better performance
+        //
+//        return self.uppercased().range(of: string.uppercased()) != nil
+        return range(of: string, options: .caseInsensitive) != nil
+    }
 }
+
+"Hello, world".fuzzyContains(string: "hello")
