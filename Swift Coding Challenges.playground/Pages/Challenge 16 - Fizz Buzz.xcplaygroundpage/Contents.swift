683d4d0f898f70446884e1170c6ed79dcffaa98d
ericysze <ericysze@gmail.com>
2017-01-21 01:38:13 -0500

    Challenge 17 complete

--- Swift Coding Challenges.playground/Pages/Challenge 16 - Fizz Buzz.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Challenge 16 - Fizz Buzz.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-func FizzBuzz() {
+func challenge16a() {
     for i in 1...100 {
         if i % 3 == 0 && i % 5 == 0 {
             print("Fizz Buzz")
@@ -16,4 +16,24 @@ func FizzBuzz() {
     }
 }
 
-FizzBuzz()
+challenge16a()
+
+
+// this function has a 33% performance increase because nesting the "Fizz Buzz" case + check of i % 5 == 0 inside i % 3 == 0 saves 66 checks, avoiding to have to check i % 3 == 0 twice. 
+func challenge16b() {
+    for i in 1...100 {
+        if i % 3 == 0 {
+            if i % 5 == 0 {
+                print("Fizz Buzz")
+            } else {
+                print("Fizz")
+            }
+        } else if i % 5 == 0 {
+            print("Buzz")
+        } else {
+            print(i)
+        }
+    }
+}
+
+challenge16b()
---
+++ Swift Coding Challenges.playground/Pages/Challenge 17 - Rand Num Gen.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func challenge17(min: Int, max: Int) -> Int {
+
+    return Int(arc4random_uniform(UInt32(max - min + 1))) + min
+}
+
+challenge17(min: 1, max: 5)
+challenge17(min: 8, max: 10)
+challenge17(min: 12, max: 12)
+challenge17(min: 12, max: 18)
