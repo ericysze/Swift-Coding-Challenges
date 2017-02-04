3e31f699d57f703f750abd7554cd86b4334e1b67
ericysze <ericysze@gmail.com>
2017-02-04 01:11:46 -0500

    Wrote bubble sort from scratch again with comments to strengthen understanding

--- Swift Coding Challenges.playground/Pages/Bubble sort for fun.xcplaygroundpage/Contents.swift
+++ Swift Coding Challenges.playground/Pages/Bubble sort for fun.xcplaygroundpage/Contents.swift
@@ -5,20 +5,20 @@ import Foundation
 // inefficient bubbleSort algorithm because double for loop will force the function to keep running until it has looped through all incrementations, even if it is sorted.
 // use while loop and a flag to break out of the function when it has checked to make sure the array is sorted then break.
 func bubbleSort(arr: inout [Int]) -> [Int] {
-    
-    for _ in 0..<arr.count {
-        for i in 0..<arr.count {
-            let first = arr[i]
+    while true {
+        var sorted = false
+        for i in 1..<arr.count {
             
-            if i != arr.count - 1 {
-                let second = arr[i+1]
-                if first > second {
-                    swap(&arr[i], &arr[i+1])
-                }
+            if arr[i - 1] > arr[i] {
+                sorted = true
+                swap(&arr[i], &arr[i - 1])
             }
+
+        }
+        if !sorted {
+            break
         }
     }
-    
     return arr
 }
 var array = [42, 12, 88, 62, 63, 56, 1, 77, 88, 97, 97, 20, 45, 91, 62, 2, 15, 31, 59, 5]
@@ -50,3 +50,33 @@ func bubbleSort2(arr: inout [Int]) -> [Int] {
 
 var unsortedArr = [10, 9, 1, 8, 4, 7, 5, 6, 3, 2]
 bubbleSort2(arr: &unsortedArr)
+
+
+// write bubble sort again without built in swap function
+
+func bubbleSort3(arr: inout [Int]) -> [Int] {
+    var temp: Int
+    
+    // use a while true loop because as soon as the array is sorted, it will break
+    while true {
+        // create unsorted var set to false because 1) we don't know if the arr is sorted yet, function will run the for loop next to check if there is anything to swap
+        var unsorted = false
+        
+        // while the for loop is running, it's going to check if there's anything to swap, if there is it sets unsorted to true, that way if it exits the for loop, it won't break b/c the if statement condition is false, causing it to start over from the top and into the for loop again, if there's nothing to swap as it reaches the last element, that means the arr is sorted, and unsorted doesn't get set to true, allowing the if statement below to break out of the while loop as it !'s (not's) the false unsorted var and returns the final sorted arr
+        for i in 1..<arr.count {
+            if arr[i - 1] > arr[i] {
+                unsorted = true
+                temp = arr[i - 1]
+                arr[i - 1] = arr[i]
+                arr[i] = temp
+            }
+        }
+        
+        if !unsorted {
+            break
+        }
+    }
+    return arr
+}
+
+bubbleSort3(arr: &unsortedArr)
